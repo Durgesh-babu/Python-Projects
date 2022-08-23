ca3297d721f5815eb654793f3644299bab37ab6e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-08-23 20:11:18 +0530

    Format tic-tac-toe.py with black

--- Tic-Tac-Toe/tic-tac-toe.py
+++ Tic-Tac-Toe/tic-tac-toe.py
@@ -1,57 +1,61 @@
-board = [['1','2','3'],
-         ['4','5','6'],
-         ['7','8','9']]
-p1 = ''
-p2 = ''
+board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+p1 = ""
+p2 = ""
 tie = False
 r = 0
 c = 0
-key = 'X'
+key = "X"
+
 
 def main():
-    global p1,p2,key
+    global p1, p2, key
     p1 = input("Enter player 1 name: ")
     p2 = input("Enter player 1 name: ")
-    print('-----------------------------------------------------')
-    print(f'{p1} plays first and {p2} plays next.')
-    print('-----------------------------------------------------')
+    print("-----------------------------------------------------")
+    print(f"{p1} plays first and {p2} plays next.")
+    print("-----------------------------------------------------")
 
     while not win():
         display_board()
         position()
         win()
 
-    if key == 'O' and not tie:
-        print(f'{p1} wins!')
-    elif key == 'X' and not tie:
+    if key == "O" and not tie:
+        print(f"{p1} wins!")
+    elif key == "X" and not tie:
         print(f"{p2} wins!")
     else:
         print(f"It's a tie!")
 
+
 def win():
     global tie
     for i in range(3):
-        if (board[0][i] == board[1][i] and board[1][i] == board[2][i]) or (board[i][0] == board[i][1] and board[i][1] == board[i][2]):
+        if (board[0][i] == board[1][i] and board[1][i] == board[2][i]) or (
+            board[i][0] == board[i][1] and board[i][1] == board[i][2]
+        ):
             return True
-        if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) or (board[0][2] == board[1][1] and board[1][1] == board[2][0]):
+        if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) or (
+            board[0][2] == board[1][1] and board[1][1] == board[2][0]
+        ):
             return True
-        
+
         for i in range(3):
             for j in range(3):
-                if board[i][j] != 'X' and board[i][j] != 'O':
+                if board[i][j] != "X" and board[i][j] != "O":
                     return False
-                
+
     tie = True
     return True
 
 
 def position():
     position_var = 0
-    global r,c,key
-    if key == 'X':
-        position_var = int(input(f'{p1} enter the positon: '))
+    global r, c, key
+    if key == "X":
+        position_var = int(input(f"{p1} enter the positon: "))
     else:
-        position_var = int(input(f'{p2} enter the positon: '))
+        position_var = int(input(f"{p2} enter the positon: "))
     if position_var == 1:
         r = 0
         c = 0
@@ -69,7 +73,7 @@ def position():
         c = 1
     elif position_var == 6:
         r = 1
-        c = 2 
+        c = 2
     elif position_var == 7:
         r = 2
         c = 0
@@ -80,16 +84,19 @@ def position():
         r = 2
         c = 2
     else:
-        print('Invalid position!')
+        print("Invalid position!")
         position()
 
-    if board[r][c] != 'X' and board[r][c] != 'O':
+    if board[r][c] != "X" and board[r][c] != "O":
         board[r][c] = key
-        if key == 'X': key = 'O' 
-        else: key = 'X'
+        if key == "X":
+            key = "O"
+        else:
+            key = "X"
     else:
         print("Occupied!")
         position()
+
 
 def display_board():
     print(f"---------")
@@ -98,5 +105,6 @@ def display_board():
     print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
     print(f"---------")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
